398694b12eb1f9540e836c2a5aadb9a929367b23
AJ33T <AJ33T@DESKTOP-SEKJRM0>
2024-03-25 15:21:52 +0530

    Add project files.

---
+++ Combox Lookup/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Combox_Lookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnRemoveAt = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Uttar Pradesh",
+            "Andhra Pradesh",
+            "Maharashtra",
+            "Madhya Pradesh",
+            "Tamil Nadu",
+            "Odisa"});
+            this.comboBox1.Location = new System.Drawing.Point(170, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(278, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(86, 226);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(89, 35);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(285, 226);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(88, 35);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnRemoveAt
+            // 
+            this.btnRemoveAt.Location = new System.Drawing.Point(453, 226);
+            this.btnRemoveAt.Name = "btnRemoveAt";
+            this.btnRemoveAt.Size = new System.Drawing.Size(116, 35);
+            this.btnRemoveAt.TabIndex = 1;
+            this.btnRemoveAt.Text = "RemoveAt";
+            this.btnRemoveAt.UseVisualStyleBackColor = true;
+            this.btnRemoveAt.Click += new System.EventHandler(this.btnRemoveAt_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(210, 296);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(222, 26);
+            this.txtInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 381);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnRemoveAt);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "ComBox Lookup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnRemoveAt;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Combox Lookup/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combox_Lookup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ErrorProvider err = new ErrorProvider();
+            if (txtInput.Text != "")
+            {
+                comboBox1.Items.Add(txtInput.Text);
+                txtInput.Text = "";
+                txtInput.Focus();  
+                err.Clear();
+                err.Icon = SystemIcons.Information;
+                err.SetError(txtInput, "Success!");
+            }else
+            {
+                err.Clear();
+                err.Icon = SystemIcons.Error;
+                MessageBox.Show("Please type something");
+                err.SetError(txtInput, "Fill this");
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if(txtInput.Text != "")
+            {
+                try
+                {
+                    if (comboBox1.Items.Contains(txtInput.Text))
+                    {
+                        comboBox1.Items.Remove(txtInput.Text);
+                        MessageBox.Show("Deleted");
+                    }
+                    else MessageBox.Show("Err : " + txtInput.Text + " doesn't exists!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Err : " + ex.Message);
+                }
+
+            }else MessageBox.Show("Enter what you wanted to delete");
+        }
+
+        private void btnRemoveAt_Click(object sender, EventArgs e)
+        {
+            int index;
+            if(int.TryParse(txtInput.Text,out index))
+            {
+                index = Convert.ToInt32(txtInput.Text);
+                if(index <= comboBox1.Items.Count)
+                {
+                    comboBox1.Items.RemoveAt(index);
+                    MessageBox.Show("Deleted");
+                }
+                else 
+                { 
+                    MessageBox.Show("Err : can't find" + index + "Items");
+                }
+            }else
+            {
+                MessageBox.Show("Err : Please enter index position");
+            }
+        }
+    }
+}
